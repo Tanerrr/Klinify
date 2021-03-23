390717fa5e4239ff2bb0da57726c5d48029bc67e
sakkarakaya <mehmet-sakib.karakaya@stud.hs-hannover.de>
2021-03-23 15:42:13 +0100

    added csv file from klinikbewertungen.de

--- klinikbewertung-merge.py
+++ klinikbewertung-merge.py
@@ -3,24 +3,28 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome("/Users/MEHMET/Downloads/chromedriver")
 
-klinikliste = ["https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-augenklinik-dr-hoffmann-braunschweig",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-marienstift-braunschweig",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-kliniken-herzogin-elisabeth-braunschweig",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-goettingen",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-tiefenbrunn-rosdorf",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-friederikenstift-hannover",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-annastift-hannover",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-drk-clementinenhaus-hannover",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-sophien-klinik-hannover",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-grossburgwedel",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-lehrte",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-lindenbrunn-coppenbruegge",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-hameln",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-kreis-und-stadtkrankenhaus-alfeld",
-"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-hildesheim"]
+klinikliste = ["https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-augenklinik-dr-hoffmann-braunschweig/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-marienstift-braunschweig/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-kliniken-herzogin-elisabeth-braunschweig/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-goettingen/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-tiefenbrunn-rosdorf/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-friederikenstift-hannover/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-annastift-hannover/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-drk-clementinenhaus-hannover/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-sophien-klinik-hannover/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-grossburgwedel/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-lehrte/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-lindenbrunn-coppenbruegge/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-hameln/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-kreis-und-stadtkrankenhaus-alfeld/bewertungen?allbew#more",
+"https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-hildesheim/bewertungen?allbew#more"]
+
 
 
 names=[] #List to name of the hospitals
@@ -38,8 +42,13 @@ for x in klinikliste:
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
     name = driver.find_element_by_tag_name("h1").text
-    
-    
+    time.sleep(3)
+    try:
+        driver.find_element_by_id("ez-accept-all").click()
+    except:
+        print("no cookies")
+    time.sleep(3)
+
     for i in soup.find_all("article", attrs={"class":"bewertung"}):
         names.append(name)
         title = i.find("h2")
@@ -57,7 +66,7 @@ for x in klinikliste:
     #     ratings.append(rating["class"])
 
 df = pd.DataFrame({'Name':names, 'Department':departments, 'Date':dates, 'Title':titles, 'Review':reviews, 'Rating':ratings}) 
-df.to_csv('kliniks_reviews6.csv', index=False, encoding='utf-8')
+df.to_csv('kliniks_reviews9.csv', index=False, encoding='utf-8')
 
 # print(names)
 # print(dates)
